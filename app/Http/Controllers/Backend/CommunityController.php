2676499878a9f79dc78be3c50b001b6384c0cafe
4th-avenue <quebec706@gmail.com>
2023-12-29 20:32:59 +0900

    플래시 메시지 기능 구현

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -33,7 +33,7 @@ class CommunityController extends Controller
     public function store(CommunityStoreRequest $request)
     {
         Community::create($request->validated() + ['user_id' => auth()->id()]);
-        return to_route('communities.index');
+        return to_route('communities.index')->with('message', 'Community created successfully.');
     }
 
     /**
@@ -58,7 +58,7 @@ class CommunityController extends Controller
     public function update(CommunityStoreRequest $request, Community $community)
     {
         $community->update($request->validated());
-        return to_route('communities.index');
+        return to_route('communities.index')->with('message', 'Community updated successfully.');
     }
 
     /**
@@ -67,6 +67,6 @@ class CommunityController extends Controller
     public function destroy(Community $community)
     {
         $community->delete();
-        return back();
+        return back()->with('message', 'Community deleted successfully.');
     }
 }
