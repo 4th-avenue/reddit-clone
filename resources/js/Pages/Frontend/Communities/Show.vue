a4ab2e43fbb9fb9bf25026621fca5b17bbbd6a65
4th-avenue <quebec706@gmail.com>
2024-01-02 21:12:28 +0900

    Create Post 버튼 생성・비로그인 사용자에게는 버튼 숨기기

--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,11 +1,15 @@
 <template>
     <guest-layout>
+        <div class="flex justify-end m-2 p-2">
+            <Link v-if="$page.props.auth.auth_check" href="" class="px-4 py-2 rounded-md bg-indigo-500">Create Post</Link>
+        </div>
         <h1>Community {{ community.name }} show page</h1>
     </guest-layout>
 </template>
 
 <script setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Link } from "@inertiajs/vue3";
 
 defineProps({
     community: Object(),
