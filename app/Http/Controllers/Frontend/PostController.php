6e3fb0f7a0640b94d8f215c4f907ac772dd78691
4th-avenue <quebec706@gmail.com>
2024-01-05 20:39:26 +0900

    개별글 페이지 생성

--- app/Http/Controllers/Frontend/PostController.php
+++ app/Http/Controllers/Frontend/PostController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Inertia\Inertia;
+use App\Models\Community;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PostController extends Controller
 {
     public function show($community_slug, $slug)
     {
-        dd($community_slug, $slug);
+        $community = Community::where('slug', $community_slug)->first();
+        $post = Post::where('slug', $slug)->first();
+
+        return Inertia::render('Frontend/Posts/Show', compact('community', 'post'));
     }
 }
