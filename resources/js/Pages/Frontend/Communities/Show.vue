5e9a13e74538842effa164b680433f4990cfb9a7
4th-avenue <quebec706@gmail.com>
2024-01-05 18:41:41 +0900

    Post 페이지네이션 기능 구현・사이드바 공간 만들기

--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,14 +1,24 @@
 <template>
     <guest-layout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">r/{{ community.name }}</h2>
-            <div class="flex justify-end m-2 p-2">
+            <div class="flex justify-between m-2 p-2">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">r/{{ community.name }}</h2>
                 <Link v-if="$page.props.auth.auth_check" :href="route('communities.posts.create', community.slug)" class="px-4 py-2 rounded-md font-semibold text-xs text-white uppercase bg-indigo-500 hover:bg-indigo-300">Create Post</Link>
             </div>
         </template>
 
-        <section class="m-2 p-2">
-            <PostCard v-for="post in posts.data" :post="post" :community="community.name" :key="post.id" />
+        <section class="flex md:flex-row m-2 p-2">
+            <div class="w-8/12">
+                <PostCard v-for="post in posts.data" :post="post" :community="community.name" :key="post.id" />
+                <div class="mt-4 p-2">
+                    <Pagination :links="posts.meta.links" />
+                </div>
+            </div>
+            <div class="w-4/12 p-4">
+                <div class="m-2 p-2 bg-slate-500 text-white">
+                    <h2>Latests Communities</h2>
+                </div>
+            </div>
         </section>
     </guest-layout>
 </template>
@@ -17,6 +27,7 @@
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import { Link } from "@inertiajs/vue3";
 import PostCard from "@/Components/PostCard.vue";
+import Pagination from "@/Components/Pagination.vue";
 
 defineProps({
     community: Object,
