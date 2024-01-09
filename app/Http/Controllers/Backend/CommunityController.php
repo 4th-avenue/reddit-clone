c0903afd467b1329bac2f2dfc0f40ff720c95c01
4th-avenue <quebec706@gmail.com>
2024-01-09 14:36:44 +0900

    다른 사용자의 글을 수정이나 삭제할 수 있었던 문제 해결 (Policy 생성)

--- app/Http/Controllers/Backend/CommunityController.php
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -53,6 +53,7 @@ class CommunityController extends Controller
      */
     public function edit(Community $community)
     {
+        $this->authorize('update', $community);
         return Inertia::render('Communities/Edit', compact('community'));
     }
 
@@ -61,6 +62,7 @@ class CommunityController extends Controller
      */
     public function update(CommunityStoreRequest $request, Community $community)
     {
+        $this->authorize('update', $community);
         $community->update($request->validated());
         return to_route('communities.index')->with('message', 'Community updated successfully.');
     }
@@ -70,6 +72,7 @@ class CommunityController extends Controller
      */
     public function destroy(Community $community)
     {
+        $this->authorize('delete', $community);
         $community->delete();
         return back()->with('message', 'Community deleted successfully.');
     }
