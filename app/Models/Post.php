0193a8641fb022b4d4319a311c3dfdc19977ecb0
4th-avenue <quebec706@gmail.com>
2024-01-09 16:36:32 +0900

    Comment 컨트롤러 생성과 Route・모델 릴레이션 설정

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Comment;
+use App\Models\Community;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -36,5 +38,15 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function community()
+    {
+        return $this->belongsTo(Community::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Frontend\PostController;
 use App\Http\Controllers\Backend\CommunityController;
+use App\Http\Controllers\Frontend\PostCommentController;
 use App\Http\Controllers\Backend\CommunityPostController;
 use App\Http\Controllers\Frontend\CommunityController as FrontendCommunityController;
 
@@ -20,6 +21,7 @@ Route::get('/', function () {
 
 Route::get('/r/{slug}', [FrontendCommunityController::class, 'show'])->name('frontend.communities.show');
 Route::get('/r/{community_slug}/posts/{post:slug}', [PostController::class, 'show'])->name('frontend.communities.posts.show');
+Route::post('/r/{community_slug}/posts/{post:slug}/comments', [PostCommentController::class, 'store'])->name('frontend.posts.comments');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::resource('/communities', CommunityController::class);
