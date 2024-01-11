8d6763df4c9de1686d8f1cd55e77c8a429dbe630
4th-avenue <quebec706@gmail.com>
2024-01-11 21:50:49 +0900

    PostVote 디자인 변경・개별글 페이지에 PostVote 추가

--- resources/js/Components/PostVote.vue
+++ resources/js/Components/PostVote.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-12 flex flex-col m-2 p-2">
-        <UpVoteLink :slug="post.slug" :class="{'text-blue-600': post.postVotes[0] && post.postVotes[0].vote == 1}" />
-        <div class="p-2 my-2 bg-blue-300 rounded-md">{{ post.votes }}</div>
-        <DownVoteLink :slug="post.slug" :class="{'text-red-600': post.postVotes[0] && post.postVotes[0].vote == -1}" />
+    <div class="w-12 flex flex-col h-full p-2 bg-gray-200 text-center">
+        <UpVoteLink :slug="post.slug" :class="{'text-gray-400': !(post.postVotes[0] && post.postVotes[0].vote == 1), 'text-blue-600': post.postVotes[0] && post.postVotes[0].vote == 1}" />
+        <div class="p-2 my-2 font-bold" :class="{'text-blue-600': post.postVotes[0] && post.postVotes[0].vote == 1, 'text-red-600': post.postVotes[0] && post.postVotes[0].vote == -1}">{{ post.votes }}</div>
+        <DownVoteLink :slug="post.slug" :class="{'text-gray-400': !(post.postVotes[0] && post.postVotes[0].vote == -1), 'text-red-600': post.postVotes[0] && post.postVotes[0].vote == -1}" />
     </div>
 </template>
 
